b0dceef41d3d41a352247fb332e1f01fb1dab750
MSreekari <msreekari11@gmail.com>
2025-07-03 12:23:54 +0530

    Modified a file

--- frontend/src/components/follow.jsx
+++ frontend/src/components/follow.jsx
@@ -4,14 +4,19 @@ import instagram from '../assets/Instagram.png';
 import linkedin from '../assets/Linkedin.png';
 import youtube from '../assets/Youtube.png';
 import X from '../assets/X.png';
-import medium from '../assets/Medium.png';
+// import medium from '../assets/Medium.png';
 
 export default function Follow() {
   return (
     <div className="follow-content-section-wrapper">
       <div className="follow-container">
       <span>Follow</span>
-      <button className='icom-wrapper'><img src={X} className="social-icon" alt="Twitter"/></button>
+      <button className='icom-wrapper'><img src={X} className="social-icon" alt="Twitter" onClick={() => {
+        window.open(
+          'https://x.com/misselectro1729',
+          '_blank'
+        );
+      }}/></button>
       <button className='icom-wrapper'><img src={facebook} className="social-icon" alt="Facebook" /></button>
     <button
       className='icom-wrapper'
@@ -30,8 +35,13 @@ export default function Follow() {
       '_blank'
     )
   }/></button>
-      <button className='icom-wrapper'><img src={medium} className="social-icon" alt="Medium" /></button>
-      <button className='icom-wrapper'><img src={youtube} className="social-icon" alt="YouTube" /></button>
+      {/* <button className='icom-wrapper'><img src={medium} className="social-icon" alt="Medium" /></button> */}
+      <button className='icom-wrapper'><img src={youtube} className="social-icon" alt="YouTube" onClick={() =>
+    window.open(
+      'https://www.youtube.com/@miss_electronics',
+      '_blank'
+    )
+  }/></button>
     </div>
     </div>
   );
